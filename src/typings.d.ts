b86d3441a58652064426d0f061f6c16553f44c43
Jobin Lawrance <jobinlawrance@gmail.com>
2017-03-06 02:40:25 +0530

    workaround for file not found typescript error

---
+++ src/typings.d.ts
@@ -0,0 +1,7 @@
+/**
+ * The typescript compiler (tsc) looks at all those .vue files
+ * and starts cribbing like crazy
+ * This line makes it keep quiet about those .vue files
+ * Comment this line out and check the app.ts file ;)
+ */
+declare module "*.vue"
