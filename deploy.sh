b99b583d854f5be78207cffa2546b0bb2d8e680d
Jobin Lawrance <jobinlawrance@gmail.com>
2017-03-10 15:14:00 +0530

    make a custom deploy script instead of using gh-pages for Travis

---
+++ deploy.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+# # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+#     echo "Skipping deploy; Either a pull request or chages to non master branch"
+#     exit 0
+# fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO out
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean out existing contents
+rm -rf out/**/* || exit 0
+
+# Run our compile script
+npm run build
+
+# Copy all the build files to out folder 
+cp dist/* out/
+
+# Now let's go have some fun with the cloned repo
+cd out
+git config user.name "Travis CI"
+git config user.email "jobinlawrance@gmail.com"
+
+# decrypt private ssh key using travis environment variables
+openssl aes-256-cbc -K $encrypted_5ce7885c22c1_key -iv $encrypted_5ce7885c22c1_iv -in travis_vue_news_rsa.enc -out travis_vue_news_rsa -d
+chmod 600 travis_vue_news_rsa
+eval `ssh-agent -s`
+ssh-add travis_vue_news_rsa
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
