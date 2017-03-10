9fb3c3ba0324ba906add20b1057f8d3dce74ebcb
Jobin Lawrance <jobinlawrance@gmail.com>
2017-03-10 15:22:34 +0530

    fix travis_vue_news_rsa.enc not found in Travis build

--- deploy.sh
+++ deploy.sh
@@ -36,8 +36,8 @@ cd out
 git config user.name "Travis CI"
 git config user.email "jobinlawrance@gmail.com"
 
-# decrypt private ssh key using travis environment variables
-openssl aes-256-cbc -K $encrypted_5ce7885c22c1_key -iv $encrypted_5ce7885c22c1_iv -in travis_vue_news_rsa.enc -out travis_vue_news_rsa -d
+# decrypt private ssh key using travis environment variables and add to ssh agent
+openssl aes-256-cbc -K $encrypted_5ce7885c22c1_key -iv $encrypted_5ce7885c22c1_iv -in ../travis_vue_news_rsa.enc -out travis_vue_news_rsa -d
 chmod 600 travis_vue_news_rsa
 eval `ssh-agent -s`
 ssh-add travis_vue_news_rsa
